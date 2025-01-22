01336c857b0d7d52431ceeeca0525c10ef5425bd
rtmincic <mincic92@gmail.com>
2025-01-22 15:50:57 -0700

    Added Leafnode and tested it to make it work

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -20,5 +20,19 @@ class HTMLNode():
         if self.tag != None and self.value != None and self.children != None and self.props != None:
             return f"HTMLNode(tag={self.tag}, value={self.value}, children={self.children}, props={self.props})"
         
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props=None):
+        super().__init__(tag, value, children=[], props=props)
+
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("Value should have a value")
+        if self.tag == None:
+            return f"{self.value}"
+        if self.props is not None:
+            return f"<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>"
+        return f"<{self.tag}>{self.value}</{self.tag}>"
+        
+        
         
         
---
+++ src/test_leafnode.py
@@ -0,0 +1,26 @@
+import unittest
+
+from htmlnode import HTMLNode, LeafNode
+
+class TestLeafNode(unittest.TestCase):
+    def test_leaf_to_html(self):
+        test_prop = {
+                        "href": "https://www.boot.dev",
+                        "target": "_blank"
+                     }
+        node = LeafNode("p", "This is a paragraph tags value", test_prop)
+        self.assertEqual(node.to_html(), "<p href=\"https://www.boot.dev\" target=\"_blank\">This is a paragraph tags value</p>")
+
+    def test_leaf_with_no_props(self):
+        node = LeafNode("h1", "This is header 1's value")
+        self.assertEqual(node.to_html(), "<h1>This is header 1's value</h1>")
+
+    def test_no_tag(self):
+        node = LeafNode(tag=None, value="This is a tagless value")
+        self.assertEqual(node.to_html(), "This is a tagless value")
+
+    def test_no_value(self):
+        with self.assertRaises(ValueError):
+            node = LeafNode("p" , None)
+            node.to_html()
+        
